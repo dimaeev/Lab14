12f8ceca24c9eacf7022792a9176c7779bfd078d
dimaeev <alexeeev.dima@gmail.com>
2025-02-27 18:48:09 +0700

    Base Document class with properties and methods

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Document
+{
+    public string Name { get; set; }
+    public string Author { get; set; }
+    public string Keywords { get; set; }
+    public string Topic { get; set; }
+    public string FilePath { get; set; }
+
+    public Document(string name, string author, string keywords, string topic, string filepath)
+    {
+        Name = name;
+        Author = author;
+        Keywords = keywords;
+        Topic = topic;
+        FilePath = filepath;
+    }
+
+    public virtual void GetInfo()
+    {
+        Console.WriteLine($"Документ: {Name}");
+        Console.WriteLine($"Автор: {Author}");
+        Console.WriteLine($"Ключевые слова: {Keywords}");
+        Console.WriteLine($"Тематика: {Topic}");
+        Console.WriteLine($"Путь к файлу: {FilePath}");
+    }
+}
